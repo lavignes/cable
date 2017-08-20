4a7749100dca62ba18e26e7e284b1ad3c8d030a7
Scott LaVigne <pyrated@gmail.com>
2017-08-19 22:02:06 -0700

    Add some Cbl2d methods

--- libcable/include/cable/math/2d.h
+++ libcable/include/cable/math/2d.h
@@ -9,6 +9,27 @@
 
 CBL_EXTERN_BEGIN
 
+CBL_INLINE Cbl2d cbl2dDot(Cbl2d lhs, Cbl2d rhs) {
+    const Cbl2d mul = cbl2dMul(lhs, rhs);
+    return cbl2dAdd(cbl2dSplatX(mul), cbl2dSplatY(mul));
+}
+
+CBL_INLINE Cbl2d cbl2dLength(Cbl2d v2d) {
+    return cbl2dSqrt(cbl2dDot(v2d, v2d));
+}
+
+CBL_INLINE Cbl2d cbl2dLengthFast(Cbl2d v2d) {
+    return cbl2dSqrtFast(cbl2dDot(v2d, v2d));
+}
+
+CBL_INLINE Cbl2d cbl2dSquareLength(Cbl2d v2d) {
+    return cbl2dDot(v2d, v2d);
+}
+
+CBL_INLINE Cbl2d cbl2dNormalize(Cbl2d v2d) {
+    return cbl2dMul(v2d, cbl2dRecipSqrt(cbl2dDot(v2d, v2d)));
+}
+
 CBL_EXTERN_END
 
 #endif // CBL_MATH_2D_H_
